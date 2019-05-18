6cab15a82e5dce2c17439edee8ef754cbbf6e288
Mike Bourbeau <borbs727@gmail.com>
2019-05-18 13:59:54 -0400

    stuffs

---
+++ Assets/VoxelTerrain/QuadTreeComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadtreeComponent : MonoBehaviour
+{
+    public float size = 5;
+    public int depth = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos() {
+        var quadtree = new Quadtree<int>(this.transform.position, size, depth);
+        
+        DrawNode(quadtree.GetRoot());
+    }
+
+    private Color minColor = new Color(1, 1, 1, 1f);
+    private Color maxColor = new Color(0, 0.5f, 1, 0.24f);
+
+    private void DrawNode(Quadtree<int>.QuadtreeNode<int> node, int nodeDepth = 0)
+    {
+        if (!node.IsLeaf())
+        {
+            foreach (var subnode in node.Nodes)
+            {
+                DrawNode(subnode, nodeDepth + 1);
+            }
+        }
+        Gizmos.color = Color.Lerp(minColor, maxColor, nodeDepth / (float)depth);
+        Gizmos.DrawWireCube(node.Position, new Vector3(1, 1, 0.1f) * node.Size);
+    }
+}
---
+++ Assets/VoxelTerrain/Quadtree.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum QuadtreeIndex{
+    TopLeft = 0,     //00, 
+    TopRight = 1,    //01,
+    BottomLeft = 2,  //10, 
+    BottomRight = 3, //11,
+}
+
+public class Quadtree<TType> 
+{
+    private QuadtreeNode<TType> node; // Root node
+    private int depth; // How deep the three can go (resolution) (number of subdivisions of cube)
+
+    public Quadtree(Vector2 position, float size, int depth)
+    {
+        node = new QuadtreeNode<TType>(position, size);
+        node.Subdivide(depth);
+    }
+
+    public class QuadtreeNode<TType> 
+    {
+        Vector2 position;
+        float size; // Cube, so all sizes will be identical and not require a Vector3
+        QuadtreeNode<TType>[] subNodes;
+        IList<TType> value;
+
+        public IEnumerable<QuadtreeNode<TType>> Nodes 
+        { 
+            get { return subNodes; } 
+
+        }
+
+        public QuadtreeNode(Vector2 pos, float size) 
+        {
+            position = pos;
+            this.size = size;
+        }
+        
+        public Vector2 Position 
+        { 
+            get {return position;}
+        }
+        public float Size 
+        { 
+            get {return size;}
+        }
+
+        public void Subdivide(int depth = 0)
+        {
+            subNodes = new QuadtreeNode<TType>[4];
+            for (int i = 0; i < subNodes.Length; ++i)
+            {
+                Vector2 newPos = position;
+                if ((i & 2) == 2)
+                {
+                    newPos.y -= size * 0.25f;
+                }
+                else
+                {
+                    newPos.y += size * 0.25f;
+                }
+
+                if ((i & 1) == 1)
+                {
+                    newPos.x += size * 0.25f;
+                }
+                else
+                {
+                    newPos.x -= size * 0.25f;
+                }
+
+                subNodes[i] = new QuadtreeNode<TType>(newPos, size * 0.5f);
+                if (depth > 0)
+                {
+                    subNodes[i].Subdivide(depth-1);
+                }
+            }
+        }
+
+        public bool IsLeaf()
+        {
+            return subNodes == null;
+        }
+    }
+
+    private int GetIndexOfPosition(Vector2 lookupPosition, Vector2 nodePosition)
+    {
+        int index = 0;
+
+        index |= lookupPosition.y > nodePosition.y ? 2 : 0;
+        index |= lookupPosition.x > nodePosition.x ? 1 : 0;
+
+        return index;
+    }
+
+    public QuadtreeNode<TType> GetRoot()
+    {
+        return node;
+    }
+}
+
